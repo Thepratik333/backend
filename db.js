09a3fb74d996c451d9a1c83d6c11d5809f811200
Thepratik333 <pratikpanchal858@gmail.com>
2024-03-05 16:22:06 +0530

    db update

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = "mongodb://127.0.0.1:27017/pratik";
+const mongoUri = "mongodb://localhost:27017/pratik";
 
 const connectmongo = () => {
   mongoose.connect(mongoUri,{
